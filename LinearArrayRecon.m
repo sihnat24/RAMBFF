cd98f8d8638c2a55e6fe6c35e3623544e8a665b6
cvoloshen <158849871+cvoloshen@users.noreply.github.com>
2024-04-29 10:17:30 -0400

    Add files via upload

--- LinearArrayRecon.m
+++ LinearArrayRecon.m
@@ -10,7 +10,6 @@
 
 load('AbdominalMap3.mat'); % From Link In GitHub Repository 
 %% Define Parameters and Points to Focus At 
-tic
 dBrange = [-40, 0]; c = 1500; 
 wave_length = c/1.0e6; 
 [X,Y,Z] = meshgrid(x,0,z);
@@ -18,20 +17,22 @@ foc_pts = [X(:) Y(:) Z(:)]; % Set up grid of focus points
 
 txAptPos = rxAptPos; % Transmit and receive at same Transducer Points
 nelement = size(rxAptPos,1);
+
 %% Reconstruct Images
 tic
 focData = bfm_fs_fast(time,scat,foc_pts,rxAptPos,txAptPos,c); % Reconstruct with constant speed of sound
 focDataVI = bfm_fs_fastVI(x,z,time,scat,foc_pts,rxAptPos,txAptPos,C); % Reconstruct with sound speed map
 toc
 
-img_h = reshape(focData, [numel(x), numel(z)])'; % plot both images for comparison
-figure(1)
+%% Plot Both Images for Comparison
+img_h = reshape(focData, [numel(x), numel(z)])'; 
+figure(1) % This image is with a constant speed of sound, c 
 imagesc(1000*x, 1000*z, 20*log10(abs(img_h)/max(abs(img_h(:)))), dBrange); 
 axis image; xlabel('Lateral [mm]'); ylabel('Axial [mm]');
 title('Unadjusted Sound Speed DAS Beamforming'); colormap(gray); colorbar();
 
 img_h2 = reshape(focDataVI, [numel(x), numel(z)])'; 
-figure(2)
+figure(2) % This image is with the varying sound speed map
 imagesc(1000*x, 1000*z, 20*log10(abs(img_h2)/max(abs(img_h2(:)))), dBrange); 
 axis image; xlabel('Lateral [mm]'); ylabel('Axial [mm]');
 title('Adjusted Sound Speed DAS Beamforming'); colormap(gray); colorbar();
